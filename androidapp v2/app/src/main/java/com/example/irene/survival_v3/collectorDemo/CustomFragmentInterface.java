6ec26a82901771373fe31f187263ccf2f60ea8a7
birene <birene@student.ethz.ch>
2017-03-14 10:52:08 +0100

    PageFragment visibity changed only when swipe

---
+++ androidapp v2/app/src/main/java/com/example/irene/survival_v3/collectorDemo/CustomFragmentInterface.java
@@ -0,0 +1,10 @@
+package com.example.irene.survival_v3.collectorDemo;
+
+/**
+ * Created by Irene on 14/03/2017.
+ */
+
+public interface CustomFragmentInterface {
+    void fragmentBecameVisible();
+
+}
